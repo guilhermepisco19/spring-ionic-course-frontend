8b1e8ac647bb00b8a067ee0010216c684624a33a
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2020-10-10 14:31:25 +0100

    Showing client image and data in profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CategoryService } from '../services/domain/category.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { ClientService } from '../services/domain/client.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +34,8 @@ import { StorageService } from '../services/storage.service';
     CategoryService,
     ErrorInterceptorProvider,
     AuthService,
-    StorageService
+    StorageService,
+    ClientService
   ]
 })
 export class AppModule {}
