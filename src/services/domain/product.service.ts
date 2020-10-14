4eace75246eae0b72fa7e66deaee5fa6c73dbdb2
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2020-10-15 00:53:46 +0100

    Loading products images

--- src/services/domain/product.service.ts
+++ src/services/domain/product.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
 import { API_CONFIG } from "../../config/api.config";
 
 
@@ -11,5 +12,10 @@ export class ProductService {
 
     findByCategory(category_id : string) {
         return this.http.get(`${API_CONFIG.baseUrl}/products?categorias=${category_id}`);
+    }
+
+    getSmallImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url, {responseType : 'blob'});
     }
 }
