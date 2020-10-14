a7cf533f8f7114fac7f50213e865000c612a38ce
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2020-10-15 00:25:04 +0100

    Retrieving products for a given category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClientService } from '../services/domain/client.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
+import { ProductService } from '../services/domain/product.service';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoryService,
+    ProductService,
     AuthInterceptorProvider,
     ErrorInterceptorProvider,
     AuthService,
