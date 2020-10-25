e272e1903c8616f68f03a3fcf9ccd7fbc1b082d8
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2020-10-25 20:15:43 +0000

    Testing the application in android device

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://spring-ionic-course-backend.herokuapp.com",
     bucketBaseUrl: "https://gp19-curso-spring-ionic.s3.eu-west-3.amazonaws.com"
 }
